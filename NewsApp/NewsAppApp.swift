62802cae5e2c1845b8a956a9faf038b8700757c0
Kavindu-Randimal <kavi.randimal97@gmail.com>
2022-09-27 21:41:53 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Randimal Geeganage on 2022-09-27.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
